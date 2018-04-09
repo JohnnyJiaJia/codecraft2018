1a7ec1d4e380aa1bd1a95cdfdef5df91aea1d0a0
octoplus <932499838@qq.com>
2018-04-09 15:25:03 +0800

    update

--- src/ecs/predict.py
+++ src/ecs/predict.py
@@ -22,7 +22,7 @@ def get_average(l, k):
     return s / j
 
 
-def predict(ecs_lines, pred_start, pred_end, flavors):
+def ma_predict(ecs_lines, pred_start, pred_end, flavors):
     result = dict()
     train_start = datetime.datetime.strptime(ecs_lines[0].strip().split("\t")[-1], "%Y-%m-%d %H:%M:%S")
     train_end = datetime.datetime.strptime(ecs_lines[-1].strip().split("\t")[-1], "%Y-%m-%d %H:%M:%S")
@@ -38,4 +38,34 @@ def predict(ecs_lines, pred_start, pred_end, flavors):
 
     for f in flavors:
         result[f] = get_average(d[f], predict_days)
+
     return result
+
+
+def total_num(ecs_lines):
+    d = dict()
+    for l in ecs_lines:
+        id, k, t = l.strip().split("\t")
+        if k in d:
+            d[k] += 1
+        else:
+            d[k] = 1
+    return d
+
+
+def ta_predict(ecs_lines, pred_start, pred_end, flavors):
+    result = dict()
+    train_start = datetime.datetime.strptime(ecs_lines[0].strip().split("\t")[-1], "%Y-%m-%d %H:%M:%S")
+    train_end = datetime.datetime.strptime(ecs_lines[-1].strip().split("\t")[-1], "%Y-%m-%d %H:%M:%S")
+    total_days = (train_end - train_start).days + 1
+    predict_days = (pred_end - pred_start).days + 1
+
+    d = total_num(ecs_lines)
+
+    for f in flavors:
+        result[f] = int(d[f] * 1.0 / total_days * predict_days)
+    return result
+
+
+def predict(ecs_lines, pred_start, pred_end, flavors):
+    return ta_predict(ecs_lines, pred_start, pred_end, flavors)
